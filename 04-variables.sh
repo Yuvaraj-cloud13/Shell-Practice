cf850a681ac0d2dfe541731fd46835064817414c
yuvaraj <yuvarajhv1311@gmail.com>
2026-05-24 16:40:40 +0530

    hello world

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+read USER_NAME
+
+echo "username is $USER_NAME"
+
+
+echo "please enter you password"
+read -s PASSWORD
+
+echo "password is $PASSWORD"
