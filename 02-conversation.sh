b36813087f0c0e5644d739ff6d426d3d4f7a96b3
yuvaraj <yuvarajhv1311@gmail.com>
2026-05-24 16:38:39 +0530

    hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Russia
+Person2=Ukraine
+
+echo "$Person1: Hi $Person2 we are going to blast you"
+echo "$Person2: Yes, please. you are welcome"
+echo "$Person1: hey $Person2 i am serious"
+echo "$Person2: hey $Person1 we are also serious"
