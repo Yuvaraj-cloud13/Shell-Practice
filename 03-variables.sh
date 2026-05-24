7e223a792ef30801c48acc116e5a077965392449
yuvaraj <yuvarajhv1311@gmail.com>
2026-05-24 16:39:59 +0530

    hello world

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2 we are going to blast you"
+echo "$Person2: Yes, please. you are welcome"
+echo "$Person1: hey $Person2 i am serious"
+echo "$Person2: hey $Person1 we are also serious"
+ 
