6c1091efc9b0f53d82e400361dca9b6ec7d11938
yuvaraj <yuvarajhv1311@gmail.com>
2026-05-24 20:28:08 +0530

    hello world

---
+++ 09-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+#check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+
+echo "installing mysql"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "installing mysql is ...failed"
+    exit 1
+else 
+    echo "installing mysql is ...success"
+fi
